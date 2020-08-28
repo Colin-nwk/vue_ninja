2fbc41bf480eaa952b2df4e671f93812e762c265
Colin-nwk <collinsnwoko05@gmail.com>
2020-08-28 18:36:11 +0100

    modifiers

---
+++ app.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: "#app",
+  data: {
+    title: "Becoming a view ninja",
+    name: "Ryu",
+  },
+  methods: {
+    logMessage() {
+      console.log("modifiers");
+    },
+  },
+});
